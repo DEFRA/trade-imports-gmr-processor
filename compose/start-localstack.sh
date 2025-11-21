2c300ec0fc867dd743b7f7a3193396cae718d799
D Klemm <184640818+d-ee-klemm@users.noreply.github.com>
2025-11-21 12:33:06 +0000

    Refactor localstack init

--- compose/start-localstack.sh
+++ compose/start-localstack.sh
@@ -4,21 +4,22 @@ export AWS_DEFAULT_REGION=eu-west-2
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
 
-QUEUE_NAME="trade_imports_data_upserted_gmr_processor_gto"
+QUEUE_NAMES=(
+    "trade_imports_data_upserted_gmr_processor_gto"
+)
 SQS_ENDPOINT_URL="http://localhost:4566"
 
-aws --endpoint-url="$SQS_ENDPOINT_URL" \
-    sqs create-queue \
-    --queue-name "$QUEUE_NAME"
-
 is_queue_ready() {
-    [ "$(aws --endpoint-url="$SQS_ENDPOINT_URL" sqs list-queues --region "$AWS_REGION" \
-        --query "QueueUrls[?contains(@, '$1')] | [0] != null")" = "true" ]
+    [ "$(aws --endpoint-url="$SQS_ENDPOINT_URL" sqs list-queues --region "$AWS_REGION" --query "QueueUrls[?contains(@, '$1')] | [0] != null")" = "true" ]
 }
 
-while ! is_queue_ready "$QUEUE_NAME"; do
-    echo "Waiting until ready"
-    sleep 1
+for queue in "${QUEUE_NAMES[@]}"; do
+    aws --endpoint-url="$SQS_ENDPOINT_URL" sqs create-queue --queue-name "$queue"
+
+    while ! is_queue_ready "$queue"; do
+        echo "Waiting for $queue to be ready"
+        sleep 1
+    done
 done
 
 touch /tmp/ready
