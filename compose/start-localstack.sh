9eeb2f74470e4473bb5740e19096217985316df4
D Klemm <184640818+d-ee-klemm@users.noreply.github.com>
2025-11-21 12:18:52 +0000

    Sonar warning

--- compose/start-localstack.sh
+++ compose/start-localstack.sh
@@ -9,15 +9,9 @@ aws --endpoint-url=http://localhost:4566 \
     --queue-name trade_imports_data_upserted_gmr_processor_gto
 
 function is_ready() {
-    if list_queues="$(aws --endpoint-url=http://localhost:4566 \
+    list_queues="$(aws --endpoint-url=http://localhost:4566 \
     sqs list-queues --region eu-west-2 --query "QueueUrls[?contains(@, 'trade_imports_data_upserted_gmr_processor_gto')] | [0] != null"
-    )"; then
-        if [[ "$list_queues" == "true" ]]; then
-            return 0
-        fi
-    fi
-
-    return 1
+    )" && [[ "$list_queues" == "true" ]]
 }
 
 while ! is_ready; do
