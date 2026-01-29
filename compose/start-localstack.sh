63d77215cd8b051481c0ca401e5736d86971b3b6
Phil Morgan <phil@psmorgan.co.uk>
2026-01-29 09:05:08 +0000

    BAU: Update GtoMatchedGmrsQueueConsumer queue name (#49)
    The name of this queue did not match the format of the others
    (gmrs_gmr_processor_X).
    CDP has updated the queue name and this change follows to update it.

--- compose/start-localstack.sh
+++ compose/start-localstack.sh
@@ -7,9 +7,9 @@ export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
 
 QUEUE_NAMES=(
-    "trade_imports_matched_gmrs_gmr_processor_eta"
     "trade_imports_data_upserted_gmr_processor_gto"
-    "trade_imports_matched_gmrs_processor_gto"
+    "trade_imports_matched_gmrs_gmr_processor_eta"
+    "trade_imports_matched_gmrs_gmr_processor_gto"
     "trade_imports_matched_gmrs_gmr_processor_match"
 )
 SQS_ENDPOINT_URL="http://localhost:4566"
